5dabee1881e0225d7d1ff3d7486e546e9459953d
Sotiris Tzamaras <sotos@tzamaras.com>
2020-03-04 11:43:27 +0200

    add function selection

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,16 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the learning rate (Good values are usually 0.01, 0.03 or 0.05)
-learning_rate = 0.05
+selection = -1         # User selection options
+functionName = ''      # Holds function name for plot label
+points = []            # List of locations
+learning_rate = 0.03   # Define the learning rate (Good values are usually 0.01, 0.03 or 0.05)
+
+text = '''
+Please select function to run gradient descent upon:
+1. 2*x*x*cos(x)-5*x
+2. arctan(x^2)
+3. x^2
+4. x^3
+5. 2*x^2-3*x+5
+0. Quit
+'''
+print(text)
+
+while True:
+    try:
+        selection = int(input("Enter your selection: "))
+        if selection <= 5 and selection >= 0:
+            # Initialize values for each function
+            if   selection == 1:
+                points.append(-1.4)
+                functionName = "2*x*x*cos(x)-5*x"
+            elif selection == 2: 
+                points.append(-2.6)
+                learning_rate = 0.2
+                functionName = "arctan(x^2)"
+            elif selection == 3: 
+                points.append(4.5)
+                learning_rate = 0.09
+                functionName = "x^2"
+            elif selection == 4: 
+                points.append(4.9)
+                learning_rate = 0.01
+                functionName = "x^3"
+            else               : 
+                points.append(-4.5)
+                learning_rate = 0.05
+                functionName = "2*x^2-3*x+5"
+            break
+        else: 
+            raise ValueError
+    except:
+        print("Wrong input specified.")
+        print(text)
+        pass
+
+# Check if selection made is exit
+if selection == 0:
+    quit()
 
 # Define f(x), the cost function to run the gradient descent algorithm on
 def f(x):
-    return 2 * x ** 2 - 3 * x + 5
+    if   selection == 1: return 2 * x * x * np.cos(x) - 5 * x
+    elif selection == 2: return np.arctan(x**2)
+    elif selection == 3: return x ** 2
+    elif selection == 4: return x ** 3
+    else               : return 2 * x ** 2 - 3 * x + 5
 
 # Define the derivative of the cost function in order to find the slope
 def df(x):
-    return 4 * x - 3
+    if   selection == 1: return 4 * x * np.cos(x) - 2 * x * x * np.sin(x) - 5
+    elif selection == 2: return (2 * x) / (1 + x ** 4)
+    elif selection == 3: return 2 * x
+    elif selection == 4: return 3 * x ** 2
+    else               : return 4 * x - 3
 
 # Create 100 points, evenly spaced, starting from -5 up to 5 on the y axis
 x = np.linspace(-5,5,100)
@@ -26,15 +83,12 @@ ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
-# List of x values over iterations
-points = [-5.]
-
 # Define how many epochs (iterations) for the algorithm to run
 for i in range(50):
     plt.clf()
     points.append(points[i] - learning_rate * df(points[i]))
     plt.grid()
-    plt.plot(x, f(x), 'r', label="2*x^2-3*x+5")                          # Plot the function
+    plt.plot(x, f(x), 'r', label=functionName)                           # Plot the function
     plt.plot([points[i]], f(points[i]), marker='o', color="blue")        # Plot the point
     plt.legend(loc='upper center')
     plt.pause(0.01)
