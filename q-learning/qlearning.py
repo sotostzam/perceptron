77c2cfeb7f7a70bc0ca8666a54aed82f1a577b8f
Sotiris Tzamaras <sotos@tzamaras.com>
2020-03-12 21:28:20 +0200

    fix movement and reset method
    This commit fixes the movement of the agent on the canvas to match
    the change in the tables. This is important as the coordinates (x,y)
    are not the same in the tables (rows, columns). In addition to this
    fix, the issue regarding the reset method is also solved. In general
    changes happened to the codebase, deleting unnecessary code.
    
    Resolves #5, resolves #6

--- q-learning/qlearning.py
+++ q-learning/qlearning.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import numpy as np
 import time
-import random
 
 # Window Interface Parameters
 window = tk.Tk()
@@ -42,181 +41,159 @@ w, h = canvas_width // 2, canvas_height // 2
 canvas = tk.Canvas(master = window, width = canvas_width, height = canvas_height, bg="grey")
 canvas.grid(row=0, column=1, sticky="nsew")
 
-####################################### FIX ME ################################################
-# Reward Table
-boundaries = np.ones((canvas_width//50, canvas_height//50)) * - 1
-# Wall objects
-wall_objects = [[0 for x in range(canvas_width//50)] for y in range(canvas_height//50)]
-max_reward = 0
 ###############################################################################################
 
 LEARNING_RATE   = 0.1
-DISCOUNT        = 0.95              # How much value future rewards over current rewards
+DISCOUNT        = 0.95      # How much value future rewards over current rewards
 EPISODES        = 25000
-INITIAL_POS     = [0, 0]
-AGENT_LOCATION  = INITIAL_POS       # Agent starting Position (columns, rows)
-GOAL_LOCATION   = [9, 9]
+ACTIONS         = np.array([0, 1, 2, 3])
 
+###############################################################################################
+
+initial_pos     = np.array([1, 0])
+agent_pos       = np.copy(initial_pos)       # Agent starting Position (columns, rows)
+goal_pos        = np.array([8, 9])
 q_table = np.random.uniform(low=-2, high=0, size = (canvas_width//50, canvas_height//50, 4))   # Needs fixing
 
-wall_objects[AGENT_LOCATION[0]][AGENT_LOCATION[1]] = canvas.create_rectangle(AGENT_LOCATION[0], AGENT_LOCATION[1], AGENT_LOCATION[0] + 50, AGENT_LOCATION[1] + 50, fill="red")
+# Initialize reward table to -1
+reward_table = np.ones((canvas_width//50, canvas_height//50)) * - 1
 
-def translateCoordinates(x, y, action):
-    global GOAL_LOCATION
-    position_x = 0
-    for i in range(0,10):
-        if x > position_x and x < position_x + 50:
-            position_y = 0
-            for j in range(0, 10):
-                if y > position_y and y < position_y + 50:
-                    if action == 1:
-                        print("Add wall to position: " + str(i) + "," + str(j))
-                        if wall_objects[i][j] == 0:
-                            wall_objects[i][j] = canvas.create_rectangle(position_x, position_y, position_x+50, position_y+50, fill="black")
-                        else:
-                            canvas.delete(wall_objects[i][j])
-                            wall_objects[i][j] = 0
-                            wall_objects[i][j] = canvas.create_rectangle(position_x, position_y, position_x+50, position_y+50, fill="black")
-                        boundaries[i, j] = -10
-                    elif action == 2:
-                        print("Add end position: " + str(i) + "," + str(j))
-                        GOAL_LOCATION = [i, j]
-                        if wall_objects[i][j] == 0:
-                            wall_objects[i][j] = canvas.create_rectangle(position_x, position_y, position_x+50, position_y+50, fill="green")
-                        else:
-                            canvas.delete(wall_objects[i][j])
-                            wall_objects[i][j] = 0
-                            wall_objects[i][j] = canvas.create_rectangle(position_x, position_y, position_x+50, position_y+50, fill="green")
-                        boundaries[i, j] = 100
-                    else:
-                        print("Remove object: " + str(i) + "," + str(j))
-                        canvas.delete(wall_objects[i][j])
-                        wall_objects[i][j] = 0
-                        boundaries[i, j] = -1
-                    break
-                else:
-                    position_y += 50
-            break
+# Canvas objects list
+#canvas_objects = [[0 for x in range(canvas_width//50)] for y in range(canvas_height//50)]
+canvas_objects = np.zeros((canvas_width//50, canvas_height//50), dtype=int)
+
+###############################################################################################
+
+canvas_objects[agent_pos[0]][agent_pos[1]] = canvas.create_rectangle(agent_pos[1] * 50, agent_pos[0] * 50, agent_pos[1] * 50 + 50, agent_pos[0] * 50 + 50, fill="red")
+canvas_objects[goal_pos[0]][goal_pos[1]] = canvas.create_rectangle(goal_pos[1] * 50, goal_pos[0] * 50, goal_pos[1] * 50 + 50, goal_pos[0] * 50 + 50, fill="green")
+
+def drawOnCanvas(event, action):
+    x = np.floor(event.x / 50).astype(int)      # Attention! This is mouse x coordinate but indicates columns in tables!
+    y = np.floor(event.y / 50).astype(int)      # Attention! This is mouse y coordinate but indicates rows in tables!
+    global goal_pos
+    if action == 1:
+        print("Add object: " + str(y) + "," + str(x))
+        if canvas_objects[y, x] == 0:
+            canvas_objects[y, x] = canvas.create_rectangle(x * 50, y * 50, x * 50 + 50, y * 50 + 50, fill="black")
         else:
-            position_x += 50
+            canvas.delete(canvas_objects[x][y])
+            canvas_objects[y, x] = 0
+            canvas_objects[y, x] = canvas.create_rectangle(x * 50, y * 50, x * 50 + 50, y * 50 + 50, fill="black")
+        reward_table[y, x] = -10
+    elif action == 2:
+        print("Add goal: " + str(y) + "," + str(x))
+        goal_pos = [y, x]
+        if canvas_objects[y, x] == 0:
+            canvas_objects[y, x] = canvas.create_rectangle(x * 50, y * 50, x * 50 + 50, y * 50 + 50, fill="green")
+        else:
+            canvas.delete(canvas_objects[y, x])
+            canvas_objects[y, x] = 0
+            canvas_objects[y, x] = canvas.create_rectangle(x * 50, y * 50, x * 50 + 50, y * 50 + 50, fill="green")
+        reward_table[y, x] = 100
+    else:
+        print("Object removed: " + str(y) + "," + str(x))
+        canvas.delete(canvas_objects[y, x])
+        canvas_objects[y, x] = 0
+        reward_table[y, x] = -1
 
-# Determine the origin by clicking
-def getAction(event, action):
-    global x0,y0
-    x0 = event.x
-    y0 = event.y
-    translateCoordinates(x0, y0, action)
-
-#mouseclick event
-canvas.bind("<Button 1>", lambda event : getAction(event, 1))
-canvas.bind("<Button 2>", lambda event : getAction(event, 2))
-canvas.bind("<Button 3>", lambda event : getAction(event, 3))
-
-def can_move(direction):
+def get_movement_availability():
+    global agent_pos
+    movement = np.array([False, False, False, False])
     try:
-        if direction == 0 and AGENT_LOCATION[0] > 0:      # Move Left
-            if boundaries[AGENT_LOCATION[0] - 1, AGENT_LOCATION[1]] > -10:
-                return True
-            else:
-                return False
-        elif direction == 1 and AGENT_LOCATION[0] < 9 :       # Move Right
-            if boundaries[AGENT_LOCATION[0] + 1, AGENT_LOCATION[1]] > -10:
-                return True
-            else:
-                return False
-        elif direction == 2 and AGENT_LOCATION[1] > 0:                                                                # Move Up
-            if boundaries[AGENT_LOCATION[0], AGENT_LOCATION[1] - 1] > -10:
-                return True
-            else:
-                return False
-        elif direction == 3 and AGENT_LOCATION[1] < 9:                                                                # Move Down
-            if boundaries[AGENT_LOCATION[0], AGENT_LOCATION[1] + 1] > -10:
-                return True
-            else:
-                return False
-        else:
-            pass
+        if agent_pos[1] > 0 and reward_table[agent_pos[0], agent_pos[1] - 1] > -10:  # Check left
+            movement[0] = True
+        if agent_pos[1] < 9 and reward_table[agent_pos[0], agent_pos[1] + 1] > -10:  # Check right
+            movement[1] = True
+        if agent_pos[0] > 0 and reward_table[agent_pos[0] - 1, agent_pos[1]] > -10:  # Check up
+            movement[2] = True
+        if agent_pos[0] < 9 and reward_table[agent_pos[0] + 1, agent_pos[1]] > -10:  # Check down
+            movement[3] = True
+        return movement
     except Exception:
         print(Exception)
 
-def move(action):
-    global max_reward
+def move(direction):
+    # Should return a new state and a reward
+
+    global agent_pos
     x, y = 0, 0
-    object_ID = wall_objects[AGENT_LOCATION[0]][AGENT_LOCATION[1]]
-    previous_location = AGENT_LOCATION
-    agent_moved = False
-    if action == 0 and can_move(0):         # Move Left
+    agent_ID = canvas_objects[agent_pos[0]][agent_pos[1]]
+    previous_location = np.copy(agent_pos)
+    agent_moved = True
+    movements_available = get_movement_availability()
+
+    if direction == 0 and movements_available[0]:         # Move Left
         x = -50
-        AGENT_LOCATION[0] -= 1
-        max_reward += boundaries[AGENT_LOCATION[0], AGENT_LOCATION[1]]
-        agent_moved = True
-    elif action == 1 and can_move(1):       # Move Right
+        agent_pos[1] -= 1
+    elif direction == 1 and movements_available[1]:       # Move Right
         x = 50
-        AGENT_LOCATION[0] += 1
-        max_reward += boundaries[AGENT_LOCATION[0], AGENT_LOCATION[1]]
-        agent_moved = True
-    elif action == 2 and can_move(2):       # Move Up
+        agent_pos[1] += 1
+    elif direction == 2 and movements_available[2]:       # Move Up
         y = -50
-        AGENT_LOCATION[1] -= 1
-        max_reward += boundaries[AGENT_LOCATION[0], AGENT_LOCATION[1]]
-        agent_moved = True
-    elif action == 3 and can_move(3):       # Move Down
+        agent_pos[0] -= 1
+    elif direction == 3 and movements_available[3]:       # Move Down
         y = 50
-        AGENT_LOCATION[1] += 1
-        max_reward += boundaries[AGENT_LOCATION[0], AGENT_LOCATION[1]]
-        agent_moved = True
-    if agent_moved:
-        canvas.move(object_ID, x, y)
-        wall_objects[previous_location[0]][previous_location[1]] = 0
-        wall_objects[AGENT_LOCATION[0]][AGENT_LOCATION[1]] = object_ID
-        rewards_text.set("Reward: " + str(max_reward))
-        return -1
+        agent_pos[0] += 1
     else:
-        max_reward -= 100
-        rewards_text.set("Reward: " + str(max_reward))
-        return -100
+        agent_moved = False
+
+    if agent_moved:
+        canvas.move(agent_ID, x, y)
+        canvas_objects[previous_location[0]][previous_location[1]] = 0
+        canvas_objects[agent_pos[0]][agent_pos[1]] = agent_ID
+        return reward_table[agent_pos[0], agent_pos[1]]
+    else:
+        return -10
 
 def reset():
-    global AGENT_LOCATION
-    canvas.delete(wall_objects[AGENT_LOCATION[0]][AGENT_LOCATION[1]])
-    AGENT_LOCATION = INITIAL_POS
-    wall_objects[AGENT_LOCATION[0]][AGENT_LOCATION[1]] = canvas.create_rectangle(AGENT_LOCATION[0], AGENT_LOCATION[1], AGENT_LOCATION[0] + 50, AGENT_LOCATION[1] + 50, fill="red")
+    global agent_pos
+    canvas.delete(canvas_objects[agent_pos[0], agent_pos[1]])
+    canvas_objects[agent_pos[0], agent_pos[1]] = 0
+    agent_pos = np.copy(initial_pos)
+    canvas_objects[agent_pos[0], agent_pos[1]] = canvas.create_rectangle(agent_pos[1] * 50, agent_pos[0] * 50, agent_pos[1] * 50 + 50, agent_pos[0] * 50 + 50, fill="red")
 
 done = False
 def train():
     btn_start.config(state="disabled")
-    global max_reward
-    action = np.argmax(q_table[AGENT_LOCATION[0]][AGENT_LOCATION[1]])
-    current_state = AGENT_LOCATION
+    current_state = np.copy(agent_pos)
     for i in range (1, EPISODES):
         episodes_text.set("Episode: " + str(i))
         max_reward = 0
-        for j in range (100, 0, -1):
+        for j in range (50, 0, -1):
             tries_text.set("Tries left: " + str(j))
+            action = np.argmax(q_table[agent_pos[0]][agent_pos[1]])
             reward = move(action)
-            new_state = tuple(q_table[AGENT_LOCATION[0]][AGENT_LOCATION[1]].astype(np.int))
-            if not done:
-                max_future_q = np.max(new_state)
-                current_q    = q_table[current_state[0]][current_state[1]][action]
-                new_q        = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
-                q_table[new_state[0]][new_state[1]][action] = new_q
-            if AGENT_LOCATION == GOAL_LOCATION:
+            max_reward += reward
+            rewards_text.set("Reward: " + str(max_reward))
+            new_state = tuple(q_table[agent_pos[0]][agent_pos[1]].astype(np.int))
+            #if not done:
+            max_future_q = np.max(new_state)
+            current_q    = q_table[current_state[0]][current_state[1]][action]
+            new_q        = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+            q_table[new_state[0]][new_state[1]][action] = new_q
+            if reward == 100:
                 print("GOAL!")
                 reset()
                 break
-            #if i == 1 or i % 100 == 0: 
-            canvas.update()
-            time.sleep(.1)
+            if i == 1 or i % 100 == 0: 
+                canvas.update()
+                time.sleep(.1)
             current_state = new_state
+        reset()
     btn_start.config(state="normal")
 
 def keypress(event):
-    if event.char == "a" or event.keycode == 37:    move(0)
-    elif event.char == "d" or event.keycode == 39:  move(1)
-    elif event.char == "w" or event.keycode == 38:  move(2)
-    elif event.char == "s" or event.keycode == 40:  move(3)
+    if   event.char == "a" or event.keycode == 37: move(0)
+    elif event.char == "d" or event.keycode == 39: move(1)
+    elif event.char == "w" or event.keycode == 38: move(2)
+    elif event.char == "s" or event.keycode == 40: move(3)
 
+# Bind Events
+canvas.bind("<Button 1>", lambda event : drawOnCanvas(event, 1))  # Place object
+canvas.bind("<Button 2>", lambda event : drawOnCanvas(event, 2))  # Place goal
+canvas.bind("<Button 3>", lambda event : drawOnCanvas(event, 3))  # Delete object
 window.bind("<Key>", keypress)
-btn_start.configure(command=train)
 
+btn_start.configure(command=train)
+btn_reset.configure(command=reset)
 window.mainloop()
