efec893471e9665842320e4d9a3d100c2e376509
Sotiris Tzamaras <sotos@tzamaras.com>
2020-03-20 18:59:45 +0200

    implement breadth-first search algorithm

--- search-algorithms/README.md
+++ search-algorithms/README.md
@@ -7,7 +7,7 @@ Given a problem with a state space, a search algorithm's purpose is to find the 
 These kinds of algorithms are also called blind search algorithms. These algorithms do not take into account the information about the evaluation of each state. So they act in the same way to solve any problem, and they only care about the timing that each state is created. Some of these algorithms are state below:
 
 * Breadth-first search
-  * [Example using a graph](https://github.com/sotostzam/artificial-intelligence/tree/search-algorithms/breadth-first-search)
+  * [Example using a graph](https://github.com/sotostzam/artificial-intelligence/tree/master/search-algorithms/breadth-first-search)
   * [Example using a binary search tree](https://github.com/sotostzam/data-structures-and-algorithms)
 * Depth-first search
   * [Example using a binary search tree](https://github.com/sotostzam/data-structures-and-algorithms)
--- search-algorithms/breadth-first-search/bfs.py
+++ search-algorithms/breadth-first-search/bfs.py
@@ -4,14 +4,45 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.edges = []
-        self.explored = False
+        self.discovered = False
+        self.parent = None
 
 class Graph:
     def __init__(self):
         self.nodes = []
+        self.start = None
+        self.target = None
+
+    def getNode(self, value):
+        for i in self.nodes:
+            if i.value == value:
+                return i
 
     def bfs_search(self, start_Node, target_Node):
-        pass
+        queue = []
+        queue.append(start_Node)
+        start_Node.discovered = True
+        while queue:
+            current = queue.pop(0)
+            if current == target_Node:
+                break
+            for edge in current.edges:
+                if edge.discovered != True:
+                    edge.discovered = True
+                    edge.parent = current
+                    queue.append(edge)
+
+        # Print the path that BFS followed
+        path = []
+        path.append(current.value)
+        while current.parent:
+            path.append(current.parent.value)
+            current = current.parent
+        path.reverse()
+        bfs_path = ""
+        for node in path:
+            bfs_path += node + " -> "
+        print("Path: " + bfs_path[0: -4])
 
 graph = Graph()
 
@@ -22,3 +53,14 @@ with open('search-algorithms/breadth-first-search/data.json') as json_file:
         for j in data[i]:
             node.edges.append(j)
         graph.nodes.append(node)
+
+    # Replace any edge with the respective node object
+    for node in graph.nodes:
+        for edge in range(0, len(node.edges)):
+            for i in graph.nodes:
+                if node.edges[edge] == i.value:
+                     node.edges[edge] = i
+
+start_Node  = graph.getNode("A")
+target_Node = graph.getNode("J")
+graph.bfs_search(start_Node, target_Node)
