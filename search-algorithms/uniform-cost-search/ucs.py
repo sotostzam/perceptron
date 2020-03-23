a317b725a8c306d247f86ddb70d5de0561381ec8
Sotiris Tzamaras <sotos@tzamaras.com>
2020-03-23 11:28:03 +0200

    edit the structure of the graph and node objects
    Changes included in this commit:
    - Removed unnecessary graph object properties
    - Edges are now being stored inside the graph object instead of each
      individual node.
    - Implemented a load function to any graph object to load a dataset
      given a path.
    - The ucs algorithm now is a standalone method that takes three
      parameters. A graph, an origin node and a target. With this change,
      it is easier to use the algorithm on different graphs, than calling
      it from within the graph.
    - Fix a bug caused when the ucs algorithm's frontier became empty,
      causing the application to get stuck in a loop.
    - Edit indentation of data.json file
    
    These changes were made so that it will be easier to run multiple
    searching algorithms on the same graph without having to create
    multiple instances of it.

--- search-algorithms/uniform-cost-search/ucs.py
+++ search-algorithms/uniform-cost-search/ucs.py
@@ -4,7 +4,6 @@ from queue import PriorityQueue
 class Node:
     def __init__(self, value):
         self.value = value
-        self.edges = []
         self.discovered = False
 
     # Rich comparison method called by x < y 
@@ -15,58 +14,78 @@ class Node:
 class Graph:
     def __init__(self):
         self.nodes = []
-        self.start = None
-        self.target = None
+        self.edges = []
 
-    # Helper function to return node object from name
-    def getNode(self, value):
-        for i in self.nodes:
-            if i.value == value:
-                return i
+    # Function to fill graph from dataset
+    def load_data(self, path):
+        with open(path) as json_file:
+            data = json.load(json_file)
+            # Loop through the vertices
+            for vertice in data:
+                node = graph.add_node(vertice)
+                # Loop through the edges
+                for edge in data[vertice]:
+                    # Loop through the keys (1 Iteration only)
+                    for node_name in edge:
+                        graph.add_edge(node, graph.add_node(node_name), edge[node_name])
 
-    def ucs_search(self, start_Node, target_Node):
-        frontier = PriorityQueue()
-        frontier.put((0, (start_Node, [])))
-        while frontier:
-            current_cost, state = frontier.get()
-            current = state[0]
-            current_path = state[1]
-            if current.discovered != True:
-                current.discovered = True
-                if current == target_Node:
-                    current_path.append(current.value)
-                    print("Path: " + str(' -> '.join(current_path)))
-                    print("Accumulated cost: " + str(current_cost))
-                    return True
-                for edge in current.edges:
-                    edge_node = edge['node']
-                    edge_cost = edge['cost']
-                    if edge_node.discovered != True:
-                        new_cost = current_cost + edge_cost
-                        new_path = current_path.copy()
-                        new_path.append(current.value)
-                        frontier.put((new_cost, (edge_node, new_path)))
-        return False
+    # Helper function to check regulate node's creation
+    def add_node(self, value):
+        for node in self.nodes:
+            if node.value == value:
+                return node
+        new_node = Node(value)
+        self.nodes.append(new_node)
+        return new_node
 
-graph = Graph()
+    # Function to add an edge to the graph
+    def add_edge(self, origin_node, target_node, cost = 0):
+        self.edges.append((origin_node, target_node, cost))
 
-with open('tour_romania.json') as json_file:
-    data = json.load(json_file)
-    for i in data:
-        node = Node(i)
-        for j in data[i]:
-            for k in j:
-                item = {"node": k, "cost": j[k]}
-                node.edges.append(item)
-        graph.nodes.append(node)
+    # Function returning a list of adjacent nodes
+    def get_neighbors(self, node):
+        neighbors = []
+        for edge in self.edges:
+            if (node == edge[0]):
+                neighbors.append((edge[1], edge[2]))
+        return neighbors
 
-    # Replace any edge with the respective node object
-    for node in graph.nodes:
-        for edge in range(0, len(node.edges)):
-            for i in graph.nodes:
-                if node.edges[edge]['node'] == i.value:
-                     node.edges[edge]['node'] = i
+    # Helper function returning node object from name
+    def get_node_obj(self, value):
+        for node in self.nodes:
+            if node.value == value:
+                return node
 
-start_Node  = graph.getNode("Arad")
-target_Node = graph.getNode("Bucharest")
-graph.ucs_search(start_Node, target_Node)
+# Define the Uniform Cost Search algorithm
+def ucs_search(graph, origin, target):
+    target_node = graph.get_node_obj(target)
+    # UCS uses a priority queue. Here priority is the lowest cost
+    frontier = PriorityQueue()        
+    # Frontier is a tuple of (priority, (node, path_to_node))                    
+    frontier.put((0, (graph.get_node_obj(origin), [])))
+    while frontier.qsize() > 0:
+        current_cost, state = frontier.get()
+        current = state[0]
+        current_path = state[1]
+        if current.discovered != True:
+            current.discovered = True
+            if current == target_node:
+                current_path.append(current.value)
+                print("Path: " + str(' -> '.join(current_path)))
+                print("Accumulated cost: " + str(current_cost))
+                return True
+
+            # Get neighbors of node and add them to frontier
+            neighbors = graph.get_neighbors(current)
+            for edge_node, cost in neighbors:
+                if edge_node.discovered != True:
+                    new_cost = current_cost + cost
+                    new_path = current_path.copy()
+                    new_path.append(current.value)
+                    frontier.put((new_cost, (edge_node, new_path)))
+    return print("Not found.")
+
+if __name__ == "__main__":
+    graph = Graph()
+    graph.load_data('tour_romania.json')
+    ucs_search(graph, "Arad", "Bucharest")
