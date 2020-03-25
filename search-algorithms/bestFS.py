e8110eddda2455217f7d73fa4b53ad2063710494
Sotiris Tzamaras <sotos@tzamaras.com>
2020-03-25 23:07:26 +0200

    modify bestFS to use a priority queue instead of a list

--- search-algorithms/bestFS.py
+++ search-algorithms/bestFS.py
@@ -1,18 +1,18 @@
+from queue import PriorityQueue
+
 def search(graph, origin, target):
     # Make sure nodes are undiscovered initially
     graph.reset_nodes()
     origin_node = graph.get_node_obj(origin)
     target_node = graph.get_node_obj(target)
-    # Hill climbing uses the node with the lowest distance to goal
-    frontier = [(origin_node, [origin_node.value], 0)]
+    # Best first search evaluates beft node like useing a priority queue (Min Heap)
+    frontier = PriorityQueue()
+    # Frontier is a tuple of (priority, (node, path_to_node, total_cost))
+    frontier.put((graph.get_distance(origin_node), (origin_node, [origin_node.value], 0)))
     while frontier:
         # Pick the best node according to the heuristic value
-        best_node = min(frontier, key = lambda t: graph.get_distance(t[0]))
-        for i in range(0, len(frontier)):
-            if frontier[i] is best_node:
-                del frontier[i]
-                break
-        current_node, path, total_cost = best_node
+        _ , state = frontier.get()
+        current_node, path, total_cost = state
         current_node.discovered = True
         if current_node is target_node:
             return path, total_cost
@@ -23,7 +23,7 @@ def search(graph, origin, target):
                     new_cost = total_cost + cost
                     new_path = path.copy()
                     new_path.append(neighbor_node.value)
-                    frontier.append((neighbor_node, new_path, new_cost))
+                    frontier.put((graph.get_distance(neighbor_node), (neighbor_node, new_path, new_cost)))
         else:
             return False
     return False
