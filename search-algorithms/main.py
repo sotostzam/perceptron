400740878b0a891a5b6866c1629b452703c9e0a7
Sotiris Tzamaras <sotos@tzamaras.com>
2020-03-27 23:26:59 +0200

    implement search algorithm selection from gui

--- search-algorithms/main.py
+++ search-algorithms/main.py
@@ -2,6 +2,7 @@ import graph, bfs, dls , ucs
 import hill_climbing as hc
 import bestFS, a_star
 import tkinter as tk
+from tkinter import ttk
 import time
 
 class gui():
@@ -10,11 +11,57 @@ class gui():
         self.window = tk.Tk()
         self.window.title("Search Algorithms")
         self.window.rowconfigure(0, minsize=500, weight=1)
-        self.window.columnconfigure(0, minsize=150, weight=1)
+        self.window.columnconfigure(0, minsize=200, weight=1)
+
+        # Main Menu
+        self.main_menu = tk.Frame(self.window)
+        self.main_menu.grid(row=0, column=0, sticky="ns")
+        #self.main_menu.rowconfigure(0, minsize=10, weight=1)
+
+        self.top_label = tk.Label(self.main_menu, text = "Choose search algorithm")
+        self.top_label.grid(row=0, column=0, sticky="nw")
+
+        self.algorithms = ['Breadth-first Search', 
+                           'Depth-first Search',
+                           'Depth Limited Search',
+                           'Iterative Deepening Search',
+                           'Uniform Cost Search',
+                           'Hill Climbing',
+                           'A*',]
+        self.combo = ttk.Combobox(self.main_menu, values = self.algorithms)
+        self.combo.bind("<<ComboboxSelected>>", lambda event : self.comboSelection(event, 1))
+        self.combo.grid(row=1, column=0, sticky="nw")
 
         # Canvas Panel Parameters
         self.canvas = tk.Canvas(master = self.window, width = width, height = height, bg="white")
-        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.canvas.grid(row=0, column=1, sticky="nsew")
+
+    # ComboBox selection callback
+    def comboSelection(self, event, item):
+        selection = self.combo.current()
+         # Make sure nodes are undiscovered initially
+        graph.reset_nodes()
+        # Algorithms return either a tuple of (found_path, total_cost) or False
+        if selection is 0:
+            path = bfs.search(graph, app, "Oradea", "Bucharest")
+        elif selection is 1:
+            path = dls.search(graph, app, "Oradea", "Bucharest")
+        elif selection is 2:
+            path = dls.search(graph, app, "Oradea", "Bucharest", depth = 3)
+        elif selection is 3:
+            path = dls.id(graph, app, "Oradea", "Bucharest")
+        elif selection is 4:
+            path = ucs.search(graph, app, "Oradea", "Bucharest")
+        elif selection is 5:
+            path = hc.search(graph, app, "Oradea", "Bucharest")
+        elif selection is 6:
+            path = bestFS.search(graph, app, "Oradea", "Bucharest")
+        else:
+            path = a_star.search(graph, app, "Oradea", "Bucharest")
+        if path is not False:
+            app.update_canvas(path[0], found = True)
+        else:
+            print("Not found")
 
     # Helper function to reset canvas items
     def reset_canvas(self):
@@ -44,29 +91,8 @@ class gui():
         if time_value is not None:
             time.sleep(time_value)
 
-
 if __name__ == "__main__":
     app = gui(800, 500)
-
     graph = graph.Graph()
     graph.load_data('tour_romania.json', app.canvas)
-
-    # Make sure nodes are undiscovered initially
-    graph.reset_nodes()
-
-    # Algorithms return either a tuple of (found_path, total_cost) or False
-    # path = bfs.search(graph, app, "Oradea", "Bucharest")
-    # path = dls.search(graph, app, "Oradea", "Bucharest")
-    # path = dls.search(graph, app, "Oradea", "Bucharest", depth = 3)
-    # path = dls.id(graph, app, "Oradea", "Bucharest")
-    # path = ucs.search(graph, app, "Oradea", "Bucharest")
-    # path = hc.search(graph, app, "Oradea", "Bucharest")
-    # path = bestFS.search(graph, app, "Oradea", "Bucharest")
-    path = a_star.search(graph, app, "Oradea", "Bucharest")
-
-    if path is not False:
-        app.update_canvas(path[0], found = True)
-    else:
-        print("Not found")
-
     app.window.mainloop()
