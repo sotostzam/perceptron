0288884f9d2c9acdf2f11162649bca9518353981
Sotiris Tzamaras <sotos@tzamaras.com>
2020-03-30 15:53:44 +0300

    implement obstacle detection and remove redundant code

--- problem-solving/02_Maze_A_Star.py
+++ problem-solving/02_Maze_A_Star.py
@@ -39,7 +39,7 @@ class Maze():
             for row in range(0, grid_h):
                 x = ((col * self.w) + (self.w * col + self.w))/2
                 y = ((row * self.h) + (self.h * row + self.h))/2
-                obj = self.canvas.create_rectangle(col * self.w, row * self.h, self.w * col + self.w, self.h * row + self.h, fill="grey")
+                obj = self.canvas.create_rectangle(col * self.w, row * self.h, self.w * col + self.w, self.h * row + self.h, fill="white")
                 new_node = Node(x, y, col, row, obj)
                 if random.uniform(0, 1) < 0.3:
                     new_node.obstacle = True
@@ -50,27 +50,35 @@ class Maze():
         for i in range(0, len(self.grid)):
             for j in range(0, len(self.grid[i])):
                 if i < grid_w - 1:
-                    self.grid[i][j].neighbors.append(self.grid[i+1][j])
+                    if not self.grid[i+1][j].obstacle:
+                        self.grid[i][j].neighbors.append(self.grid[i+1][j])
                 if i > 0:
-                    self.grid[i][j].neighbors.append(self.grid[i-1][j])
+                    if not self.grid[i-1][j].obstacle:
+                        self.grid[i][j].neighbors.append(self.grid[i-1][j])
                 if j < grid_h - 1:
-                    self.grid[i][j].neighbors.append(self.grid[i][j + 1])
+                    if not self.grid[i][j + 1].obstacle:
+                        self.grid[i][j].neighbors.append(self.grid[i][j + 1])
                 if j > 0:
-                    self.grid[i][j].neighbors.append(self.grid[i][j - 1])
+                    if not self.grid[i][j - 1].obstacle:
+                        self.grid[i][j].neighbors.append(self.grid[i][j - 1])
 
                 # Diagonals
                 if i > 0 and j > 0:
-                    self.grid[i][j].neighbors.append(self.grid[i - 1][j - 1])
+                    if not self.grid[i-1][j].obstacle or not self.grid[i][j - 1].obstacle:
+                        self.grid[i][j].neighbors.append(self.grid[i - 1][j - 1])
                 if i < grid_w - 1 and j > 0:
-                    self.grid[i][j].neighbors.append(self.grid[i + 1][j - 1])
+                    if not self.grid[i+1][j].obstacle or not self.grid[i][j - 1].obstacle:
+                        self.grid[i][j].neighbors.append(self.grid[i + 1][j - 1])
                 if i > 0 and j < grid_h - 1:
-                    self.grid[i][j].neighbors.append(self.grid[i - 1][j + 1])
+                    if not self.grid[i-1][j].obstacle or not self.grid[i][j + 1].obstacle:
+                        self.grid[i][j].neighbors.append(self.grid[i - 1][j + 1])
                 if i < grid_w - 1 and j < grid_h - 1:
-                    self.grid[i][j].neighbors.append(self.grid[i + 1][j + 1])
+                    if not self.grid[i+1][j].obstacle or not self.grid[i][j + 1].obstacle:
+                        self.grid[i][j].neighbors.append(self.grid[i + 1][j + 1])
 
     def evaluate(self, node_1, node_2):
         distance = math.sqrt((node_2.x - node_1.x)**2 + (node_2.y - node_1.y)**2)   # Euclidean distance
-        # distance = abs((node_1.x - node_2.x)) + abs((node_1.y - node_2.y))        # Manhattan distance
+        #distance = abs((node_1.x - node_2.x)) + abs((node_1.y - node_2.y))        # Manhattan distance
         return distance
 
 def a_star(maze):
@@ -92,7 +100,7 @@ def a_star(maze):
     while openSet:
         current = heapq.heappop(openSet)
 
-######################### CANVAS UPDATE ############################
+################################ CANVAS UPDATE ###################################
         for node in openSet:
             maze.canvas.itemconfig(node.obj, fill='green')
         for node in closedSet:
@@ -105,12 +113,12 @@ def a_star(maze):
             temp = temp.cameFrom
         for node in path:
             maze.canvas.itemconfig(node.obj, fill='blue')
+##################################################################################
 
         if current is goal:
             return True
         closedSet.append(current)
-#####################################################################
-
+        # maze.canvas.itemconfig(current.obj, fill='red')
         for neighbor in current.neighbors:
             if not neighbor in closedSet and not neighbor.obstacle:
                 tentative_gScore = current.gScore + maze.evaluate(current, neighbor)
@@ -124,30 +132,11 @@ def a_star(maze):
                     neighbor.fScore = neighbor.gScore + maze.evaluate(neighbor, goal)
                     neighbor.cameFrom = current
                     heapq.heappush(openSet, neighbor)
-            
-            # if tentative_gScore < neighbor.gScore:
-            #     neighbor.cameFrom = current
-            #     neighbor.gScore = tentative_gScore
-            #     neighbor.fScore = neighbor.gScore + maze.evaluate(neighbor, goal)
-            #     print(neighbor.fScore)
-            # if neighbor not in closedSet:
-            #     heapq.heappush(openSet, (neighbor.fScore, neighbor))
-            
-            # tempG = neighbor.gScore
-            # if neighbor in openSet:
-            #     if tempG < neighbor.gScore:
-            #         neighbor.gScore = tempG
-            # else:
-            #     neighbor.cameFrom = current
-            #     neighbor.gScore = tempG
-            #     neighbor.fScore = neighbor.gScore + maze.evaluate(neighbor, goal)
-            #     heapq.heappush(openSet, neighbor)
-                #maze.canvas.itemconfig(neighbor.obj, fill='green')
-
+                    # maze.canvas.itemconfig(neighbor.obj, fill='green')
         maze.canvas.update()
     return False
 
 if __name__ == "__main__":
-    maze = Maze(500, 500, 50, 50)
+    maze = Maze(800, 500, 80, 50)
     result = a_star(maze)
     maze.window.mainloop()
