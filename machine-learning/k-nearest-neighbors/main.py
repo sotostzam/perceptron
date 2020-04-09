7045eafd6ddb96fdca8b8788dbde312396a5fcfb
Sotiris Tzamaras <sotos@tzamaras.com>
2020-04-09 23:55:00 +0300

    implement function to return k neighbors based on distance

--- machine-learning/k-nearest-neighbors/main.py
+++ machine-learning/k-nearest-neighbors/main.py
@@ -3,12 +3,22 @@ import matplotlib.pyplot as plt
 import math
 
 def euclidean_distance(item_1, item_2):
-    distance = math.sqrt((item_2[0] - item_1[0])**2 + (item_2[1] - item_1[1])**2 + (item_2[2] - item_1[2])**2)
+    distance = math.sqrt((item_2[0] - item_1[0])**2 + (item_2[1] - item_1[1])**2)
     return distance
+
+def find_k_neighbors(k, item, dataset):
+    neighbor_distances = []
+    for i in range(len(dataset)):
+        if (dataset[i] == item).all():
+            continue
+        distance = euclidean_distance(item, dataset[i])
+        neighbor_distances.append((i, distance))
+    neighbor_distances.sort(key = lambda x: x[1])
+    return neighbor_distances[0: 5]
 
 def main():
     # Read and generate the third and forth feature of the dataset
-    dataset_values = np.genfromtxt('iris.data', delimiter=',', usecols=(0, 1, 2, 3))
+    dataset_values = np.genfromtxt('iris.data', delimiter=',', usecols=(0, 1))
     # Read and generate the names of the two classes
     dataset_target_names = np.genfromtxt('iris.data', delimiter=',', usecols=(4), dtype = np.str_)
     dataset_targets = np.zeros((dataset_target_names.shape[0], 1), dtype = float)
@@ -22,20 +32,24 @@ def main():
             dataset_targets[i] = float(3)
     dataset = np.column_stack((dataset_values, dataset_targets)) 
 
-    class_1 = np.where(dataset[:,4] == 1)
-    class_2 = np.where(dataset[:,4] == 2)
-    class_3 = np.where(dataset[:,4] == 3)
+    # Test item
+    test_item = find_k_neighbors(5, dataset[60], dataset)
+    for item in test_item:
+        print(dataset[item[0]])
+
+    class_1 = np.where(dataset[:,2] == 1)
+    class_2 = np.where(dataset[:,2] == 2)
+    class_3 = np.where(dataset[:,2] == 3)
 
     fig = plt.figure('k-nearest neighbors')
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
+    ax = fig.add_subplot(1, 1, 1)
 
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
 
-    ax.scatter(dataset_values[class_1, 0], dataset_values[class_1, 1], dataset_values[class_1, 2], marker='x', color='r', label='Setosa')
-    ax.scatter(dataset_values[class_2, 0], dataset_values[class_2, 1], dataset_values[class_2, 2], marker='x', color='g', label='Versicolor')
-    ax.scatter(dataset_values[class_3, 0], dataset_values[class_3, 1], dataset_values[class_3, 2], marker='x', color='b', label='Virginica')
+    ax.scatter(dataset_values[class_1, 0], dataset_values[class_1, 1], marker='x', color='r', label='Setosa')
+    ax.scatter(dataset_values[class_2, 0], dataset_values[class_2, 1], marker='o', color='g', label='Versicolor')
+    ax.scatter(dataset_values[class_3, 0], dataset_values[class_3, 1], marker='s', color='b', label='Virginica')
     ax.legend(loc='upper right')
     plt.show()
 
