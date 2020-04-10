7dd3375434d8c79bf7b1362ac0a3b97c6d8dffa2
Sotiris Tzamaras <sotos@tzamaras.com>
2020-04-10 23:28:28 +0300

    create training and test data structures

--- machine-learning/k-nearest-neighbors/main.py
+++ machine-learning/k-nearest-neighbors/main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+import math, random
 
 def euclidean_distance(item_1, item_2):
     distance = math.sqrt((item_2[0] - item_1[0])**2 + (item_2[1] - item_1[1])**2)
     return distance
 
+# Main k-neughbors classification algorithm
 def find_k_neighbors(k, item, dataset):
     neighbor_distances = []
     for i in range(len(dataset)):
@@ -14,32 +15,46 @@ def find_k_neighbors(k, item, dataset):
         distance = euclidean_distance(item, dataset[i])
         neighbor_distances.append((i, distance))
     neighbor_distances.sort(key = lambda x: x[1])
-    return neighbor_distances[0: 5]
+    return neighbor_distances[0: k]
 
 def main():
-    # Read and generate the third and forth feature of the dataset
+    # Read and generate the first and second feature of the dataset
     dataset_values = np.genfromtxt('iris.data', delimiter=',', usecols=(0, 1))
     # Read and generate the names of the two classes
     dataset_target_names = np.genfromtxt('iris.data', delimiter=',', usecols=(4), dtype = np.str_)
     dataset_targets = np.zeros((dataset_target_names.shape[0], 1), dtype = float)
-    # Iterate through the names and tranform them to two classes of -1 and 1
+    # Iterate through the names and tranform them to two classes of 0, 1 and 2
     for i in range (0, len(dataset_target_names)):
         if dataset_target_names[i] == "Iris-setosa":
-            dataset_targets[i] = float(1)
+            dataset_targets[i] = float(0)
         elif dataset_target_names[i] == "Iris-versicolor":
-            dataset_targets[i] = float(2)
+            dataset_targets[i] = float(1)
         else:
-            dataset_targets[i] = float(3)
-    dataset = np.column_stack((dataset_values, dataset_targets)) 
+            dataset_targets[i] = float(2)
 
-    # Test item
-    test_item = find_k_neighbors(5, dataset[60], dataset)
-    for item in test_item:
-        print(dataset[item[0]])
+    # Final dataset with the values as well as the classes as the last column
+    dataset = np.column_stack((dataset_values, dataset_targets))
 
-    class_1 = np.where(dataset[:,2] == 1)
-    class_2 = np.where(dataset[:,2] == 2)
-    class_3 = np.where(dataset[:,2] == 3)
+    train_data_indexes = []
+    while len(train_data_indexes) < 30:
+        first = random.randint(0, 49)
+        second = random.randint(50, 99)
+        third = random.randint(100, 149)
+        if first in train_data_indexes or second in train_data_indexes or third in train_data_indexes:
+            pass
+        else:
+            train_data_indexes.append(first)
+            train_data_indexes.append(second)
+            train_data_indexes.append(third)
+
+    train_data = np.zeros([0, dataset.shape[1]])
+    for i in range(len(train_data_indexes)):
+        train_data = np.append(train_data, ([dataset[train_data_indexes[i]]]), 0)
+    #dataset = np.delete(dataset, train_data_indexes, 0)
+
+    class_1 = np.where(dataset[:,2] == 0)
+    class_2 = np.where(dataset[:,2] == 1)
+    class_3 = np.where(dataset[:,2] == 2)
 
     fig = plt.figure('k-nearest neighbors')
     ax = fig.add_subplot(1, 1, 1)
