e99de3842f79c0ed6716020d6ccbde43a3de3763
Sotiris Tzamaras <sotos@tzamaras.com>
2020-04-11 23:07:30 +0300

    implement classification of testing data using k-neighbors algorithm

--- machine-learning/k-nearest-neighbors/main.py
+++ machine-learning/k-nearest-neighbors/main.py
@@ -8,19 +8,24 @@ def euclidean_distance(item_1, item_2):
     return distance
 
 # Main k-neughbors classification algorithm
-def find_k_neighbors(k, item, dataset):
-    neighbor_distances = []
+def k_neighbors_guess(k, item, dataset):
+    neighbor_distances = pd.DataFrame(columns = dataset.columns)
     for i in range(dataset.shape[0]):
         if (dataset.iloc[i].equals(item)):
             continue
-        distance = euclidean_distance(item, dataset.iloc[i])
-        neighbor_distances.append((i, distance))
-    neighbor_distances.sort(key = lambda x: x[1])
-    return neighbor_distances[0: k]
+        if neighbor_distances.shape[0] < k:
+            neighbor_distances = neighbor_distances.append(dataset.iloc[i], ignore_index = True)
+        else:
+            for j in range(neighbor_distances.shape[0]):
+                if euclidean_distance(item, neighbor_distances.iloc[j]) > euclidean_distance(item, dataset.iloc[i]):
+                    neighbor_distances.drop([j], inplace=True)
+                    neighbor_distances = neighbor_distances.append(dataset.iloc[i], ignore_index = True)
+    return neighbor_distances.groupby('species').size().idxmax()
 
 def main():
     # Read the iris dataset
     dataset = pd.read_csv("iris.csv")
+    training_dataset = dataset.copy()
 
     train_data_indexes = []
     while len(train_data_indexes) < 30:
@@ -34,26 +39,34 @@ def main():
             train_data_indexes.append(second)
             train_data_indexes.append(third)
 
-    test_dataset = dataset.iloc[train_data_indexes]
-    dataset.drop(train_data_indexes, inplace=True)
+    test_dataset = training_dataset.iloc[train_data_indexes]
+    training_dataset.drop(train_data_indexes, inplace=True)
+
+    class_1 = training_dataset[training_dataset['species'] == 'setosa']
+    class_2 = training_dataset[training_dataset['species'] == 'versicolor']
+    class_3 = training_dataset[training_dataset['species'] == 'virginica']
 
     correct = 0
     for i in range(test_dataset.shape[0]):
-        result = find_k_neighbors(3, test_dataset.iloc[i], dataset)
-        print(result)
+        result = k_neighbors_guess(3, test_dataset.iloc[i], dataset)
+        for j in range(dataset.shape[0]):
+            if test_dataset.iloc[i].equals(dataset.iloc[j]) and test_dataset.iloc[i].species == result:
+                correct += 1
+                break
 
-    class_1 = dataset[dataset['species'] == 'setosa']
-    class_2 = dataset[dataset['species'] == 'versicolor']
-    class_3 = dataset[dataset['species'] == 'virginica']
+        fig = plt.figure('k-nearest neighbors')
+        plt.clf()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.set_xlabel('X Label')
+        ax.set_ylabel('Y Label')
+        ax.scatter(class_1['sepal_length'], class_1['sepal_width'], marker='x', color='r', label='Setosa')
+        ax.scatter(class_2['sepal_length'], class_2['sepal_width'], marker='o', color='g', label='Versicolor')
+        ax.scatter(class_3['sepal_length'], class_3['sepal_width'], marker='s', color='b', label='Virginica')
+        ax.scatter(test_dataset.iloc[i][0], test_dataset.iloc[i][1], marker='s', color='y')
+        ax.legend(loc='upper right')
+        plt.pause(0.1)
 
-    fig = plt.figure('k-nearest neighbors')
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.scatter(class_1['sepal_length'], class_1['sepal_width'], marker='x', color='r', label='Setosa')
-    ax.scatter(class_2['sepal_length'], class_2['sepal_width'], marker='o', color='g', label='Versicolor')
-    ax.scatter(class_3['sepal_length'], class_3['sepal_width'], marker='s', color='b', label='Virginica')
-    ax.legend(loc='upper right')
+    print("Training results: " + str(correct) + "/" + str(test_dataset.shape[0]) + " (" + str(round((correct/test_dataset.shape[0])*100, 2)) + "%)")
     plt.show()
 
 if __name__ == "__main__":
