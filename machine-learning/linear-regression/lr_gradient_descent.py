0c6aae5e3daebee880b244bd5eec13a515f1f3dc
Sotiris Tzamaras <sotos@tzamaras.com>
2020-04-13 14:40:22 +0300

    plot line using stochastic gradient descent

--- machine-learning/linear-regression/lr_gradient_descent.py
+++ machine-learning/linear-regression/lr_gradient_descent.py
@@ -3,39 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Find the regression line using gradient descent
-def regression(x_values, y_values, slope, y_inter):
+def gradient_descent(x_values, y_values, m, b, learning_rate):
+    d_m = 0
+    d_b = 0
     for i in range(len(x_values)):
-        guess = slope * x_values[i] + y_inter
+        guess = m * x_values[i] + b
         error = y_values[i] - guess
-        slope += error * x_values[i] * 0.01
-        y_inter += error * 0.01
-    return slope, y_inter
+        d_m += x_values[i] * error
+        d_b += error
+    m += d_m * learning_rate
+    b += d_b * learning_rate
+    return m, b
 
 def main():
+    # Load different sets (I, II, III, IV)
     dataset = pd.read_csv("anscombes.csv", index_col=0)
     data = dataset[dataset['dataset'] == 'I']
     x = data.x.tolist()
     y = data.y.tolist()
-    x_range = np.linspace(np.min(x) - 2, np.max(x) + 2, endpoint = True)
 
-    # Initialize slope and y-intercept
-    m = 0
-    b = 0
+    # Initialize line (y = m*x + b) where m is slope and b is y-intercept
+    m = 0   # Slope
+    b = 0   # Y-intercept
+    learning_rate = 0.0001
+    iterations = 100
 
+    x_range = np.array([np.min(x) - 2, np.max(x) + 2])
     fig = plt.figure('Linear Regression using Gradient Descent')
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title("Anscombe's Quartet Set I")
+    #plt.clf()
+    ax.set_xlim(np.min(x) - 2, np.max(x) + 2)
+    ax.set_ylim(np.min(y) - 2, np.max(y) + 2)
+    ax.scatter(x, y)
 
-    for _ in range(50):
-        m, b = regression(x, y, m, b)
+    for _ in range(iterations):
+        m, b = gradient_descent(x, y, m, b, learning_rate)
         line = m * x_range + b
-
-        plt.clf()
-        ax = fig.add_subplot(1, 1, 1)
-        ax.set_title("Anscombe's Quartet Set I")
-        ax.set_xlim(np.min(x) - 2, np.max(x) + 2)
-        ax.set_ylim(np.min(y) - 2, np.max(y) + 2)
-        ax.scatter(x, y)
         ax.plot(x_range, line, linewidth=1, color='r')
-        plt.pause(0.1)
+        plt.pause(0.01)
     plt.show()
 
 if __name__ == "__main__":
