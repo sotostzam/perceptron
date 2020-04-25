c29e48a2662c961665b51a9c16d0c194c5764499
Sotiris Tzamaras <sotos@tzamaras.com>
2020-04-25 20:47:25 +0300

    modify knn algorithm
    Changes included in this commit:
    - Move knn algorithm to separate knn_class
    - Create visualize, predict, fit and accuracy methods to the model
    - Modify euclidian distance to use all dimentions
    - Plotting is now showing in three dimentions
    - Missclassified items are now shown on the plot

---
+++ machine-learning/k-nearest-neighbors/knn_classifier.py
@@ -0,0 +1,82 @@
+import math, random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+from collections import Counter
+style.use('seaborn')
+
+# Eucledean Distance using all dimentions
+def euclidean_distance(item_1, item_2):
+    distance = np.sqrt(np.sum((np.array(item_1) - np.array(item_2))**2))
+    return distance
+
+class KNN_Classifier():
+    def __init__(self, k):
+        self.fig = plt.figure('K-Nearest Neighbors')
+        self.ax = self.fig.add_subplot(1,1,1, projection='3d')
+        self.colors = {'setosa': 'm', 'versicolor': 'g', 'virginica': 'b'}
+        self.k = k
+
+    def visualize(self):
+        # Find the different species from the dataset
+        class_1 = self.dataset[self.dataset['species'] == 'setosa']
+        class_2 = self.dataset[self.dataset['species'] == 'versicolor']
+        class_3 = self.dataset[self.dataset['species'] == 'virginica']
+        self.ax.set_xlabel('Sepal length')
+        self.ax.set_ylabel('Petal length')
+        self.ax.set_zlabel('Petal width')
+        self.ax.scatter(class_1['sepal_width'], class_1['petal_length'], class_1['petal_width'], marker='p', color=self.colors['setosa'], label='Setosa')
+        self.ax.scatter(class_2['sepal_width'], class_2['petal_length'], class_2['petal_width'], marker='o', color=self.colors['versicolor'], label='Versicolor')
+        self.ax.scatter(class_3['sepal_width'], class_3['petal_length'], class_3['petal_width'], marker='d', color=self.colors['virginica'], label='Virginica')
+
+        # Get unique labels only
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+
+        self.ax.legend(by_label.values(), by_label.keys(), loc='upper left')
+        plt.show()
+
+    # Fit k-neughbors model to database
+    def fit(self, dataset, train_percent = 0.2):
+        # Randomly shuffle all rows
+        dataset = dataset.sample(frac=1).reset_index(drop=True)
+        percentage = train_percent * dataset.shape[0]
+
+        self.dataset = dataset.drop(dataset.index[-int(percentage): dataset.shape[0]], axis=0)
+        self.test_dataset = dataset.drop(dataset.index[0: -int(percentage)], 0).reset_index(drop=True)
+
+    # Get a prediction on a simple value
+    def predict(self, item):
+        neighbor_distances = []
+        for i in range(self.dataset.shape[0]):
+            # If it is the same item ignore it and move on
+            if (self.dataset.iloc[i].equals(item)):
+                continue
+            else:
+                current_distance = euclidean_distance(item, self.dataset.iloc[i].drop(['species'], axis = 0))
+                if len(neighbor_distances) < self.k:
+                    neighbor_distances.append((current_distance, self.dataset.iloc[i].species))
+                else:
+                    for j in range(len(neighbor_distances)):
+                        if neighbor_distances[j][0] > current_distance:
+                            del neighbor_distances[j]
+                            neighbor_distances.append((current_distance, self.dataset.iloc[i].species))
+                            break
+        # Use counter to count all unique species
+        counts = Counter(x[1] for x in neighbor_distances)
+        return counts.most_common(1)[0][0]
+
+    # Helper function to get the accuracy by using the test_dataset on the model
+    def accuracy(self):
+        correct = 0
+        for i in range(self.test_dataset.shape[0]):
+            item = self.test_dataset.iloc[i]
+            # Get prediction on the test sample
+            result = self.predict(item.drop(['species'], axis = 0))
+            if result == item.species:
+                correct += 1
+                self.ax.scatter(item['sepal_width'], item['petal_length'], item['petal_width'], marker='*', color=self.colors[result])
+            else:
+                self.ax.scatter(item['sepal_width'], item['petal_length'], item['petal_width'], marker='x', color='r', label='Misclassified')
+        return round((correct/self.test_dataset.shape[0])*100, 2)
--- machine-learning/k-nearest-neighbors/main.py
+++ machine-learning/k-nearest-neighbors/main.py
@@ -1,83 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import math, random
-from collections import Counter
+import knn_classifier
 
-def euclidean_distance(item_1, item_2):
-    distance = math.sqrt((item_2[2] - item_1[2])**2 + (item_2[3] - item_1[3])**2)
-    return distance
-
-# Main k-neughbors classification algorithm
-def k_neighbors_guess(k, item, dataset):
-    neighbor_distances = []
-    for i in range(dataset.shape[0]):
-        # If it is the same item ignore it and move on
-        if (dataset.iloc[i].equals(item)):
-            continue
-        else:
-            current_distance = euclidean_distance(item, dataset.iloc[i])
-            if len(neighbor_distances) < k:
-                neighbor_distances.append((current_distance, dataset.iloc[i].species))
-            else:
-                for j in range(len(neighbor_distances)):
-                    if neighbor_distances[j][0] > current_distance:
-                        del neighbor_distances[j]
-                        neighbor_distances.append((current_distance, dataset.iloc[i].species))
-                        break
-    # Use counter to count all unique species
-    counts = Counter(x[1] for x in neighbor_distances)
-    return counts.most_common(1)[0][0]
-
-def main(neighbors):
+def main():
     # Read the iris dataset
     dataset = pd.read_csv("iris.csv")
-    training_dataset = dataset.copy()
-
-    # Randomly pick 10 samples (20%) of each species and remove from dataset
-    train_data_indexes = []
-    while len(train_data_indexes) < 30:
-        first = random.randint(0, 49)
-        second = random.randint(50, 99)
-        third = random.randint(100, 149)
-        if first in train_data_indexes or second in train_data_indexes or third in train_data_indexes:
-            pass
-        else:
-            train_data_indexes.append(first)
-            train_data_indexes.append(second)
-            train_data_indexes.append(third)
-
-    test_dataset = training_dataset.iloc[train_data_indexes]
-    training_dataset.drop(train_data_indexes, inplace=True)
-
-    # Find the different species from the dataset
-    class_1 = training_dataset[training_dataset['species'] == 'setosa']
-    class_2 = training_dataset[training_dataset['species'] == 'versicolor']
-    class_3 = training_dataset[training_dataset['species'] == 'virginica']
-
-    fig = plt.figure(str(neighbors) + '-Nearest Neighbors')
-    correct = 0
-    for i in range(test_dataset.shape[0]):
-        # Get a guess on the test sample
-        result = k_neighbors_guess(neighbors, test_dataset.iloc[i], training_dataset)
-        for j in range(dataset.shape[0]):
-            if test_dataset.iloc[i].equals(dataset.iloc[j]) and test_dataset.iloc[i].species == result:
-                correct += 1
-                break
-
-        plt.clf()
-        ax = fig.add_subplot(1, 1, 1)
-        ax.set_xlabel('Petal length')
-        ax.set_ylabel('Petal width')
-        ax.scatter(class_1['petal_length'], class_1['petal_width'], marker='x', color='r', label='Setosa')
-        ax.scatter(class_2['petal_length'], class_2['petal_width'], marker='o', color='g', label='Versicolor')
-        ax.scatter(class_3['petal_length'], class_3['petal_width'], marker='^', color='b', label='Virginica')
-        ax.scatter(test_dataset.iloc[i][2], test_dataset.iloc[i][3], marker='s', color='y', label='Test sample')
-        ax.legend(loc='upper left')
-        plt.pause(0.001)
-
-    print("Training results using " + str(neighbors) + " nearest neighbors: " + str(correct) + "/" +
-          str(test_dataset.shape[0]) + " (" + str(round((correct/test_dataset.shape[0])*100, 2)) + "%)")
-    plt.show()
+    knn_cls = knn_classifier.KNN_Classifier(k = 5)
+    knn_cls.fit(dataset, train_percent = 0.2)
+    print("Confidence: " + str(knn_cls.accuracy()) + "%")
+    print("Prediction: " + str(knn_cls.predict([0,0,0,0])))
+    knn_cls.visualize()
 
 if __name__ == "__main__":
-    main(neighbors = 3)
+    main()
