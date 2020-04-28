84ef3cb4503fb6a913ce34c470c5b47df14e6b85
Sotiris Tzamaras <sotos@tzamaras.com>
2020-04-28 18:43:25 +0300

    implement naive bayes classifier training method

--- machine-learning/naive-bayes/main.py
+++ machine-learning/naive-bayes/main.py
@@ -15,13 +15,24 @@ class NaiveBayes():
         for class_item in self.classes:
             feature_counts = self.dataset[self.target_attribute].value_counts()[class_item]
             class_propability.append((class_item, feature_counts, feature_counts / total_counts))
-        print(class_propability)
+        #print(class_propability)
         
-        features_propability = []
+        self.features_propability = []
         for item in self.features:
-            # Extract propabilities of every feature
-            # print(self.dataset[item].value_counts())
-            pass
+            df = self.dataset[item]
+            values = df.unique()
+            feature_values = []
+            for value in values:
+                indexes = self.dataset[self.dataset[item] == value].index.tolist()      # Get indexes of the feature value
+                sub_matrix = self.dataset[self.target_attribute].iloc[indexes]          # Get target class values for this feature value
+                sub_matrix_counts = sub_matrix.value_counts()
+                value_classes = []
+                for f_item in range(0, sub_matrix_counts.shape[0]):
+                    f_label = sub_matrix_counts.index[f_item]
+                    f_probability = sub_matrix_counts[f_item] / total_counts
+                    value_classes.append((f_label, sub_matrix_counts[f_item], f_probability))
+                feature_values.append((value, value_classes))
+            self.features_propability.append((item, feature_values))
 
     def predict(self, attributes):
         pass
