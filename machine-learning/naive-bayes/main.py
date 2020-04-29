223312b7508c222b273494465ad865e6976b40f5
Sotiris Tzamaras <sotos@tzamaras.com>
2020-04-29 21:03:55 +0300

    fix naive bayes predict function
    This commit ensures that the predict function works as intended and
    the bug is fixed. What's more various enchancements and fixes
    have been included.
    
    Resolves #13

--- machine-learning/naive-bayes/main.py
+++ machine-learning/naive-bayes/main.py
@@ -10,11 +10,11 @@ class NaiveBayes():
 
     def train(self):
         # Find class attribute probabilities
-        self.class_propability = []
+        self.class_propability = {}
         total_counts = self.dataset[self.target_attribute].count()
         for class_item in self.classes:
             feature_counts = self.dataset[self.target_attribute].value_counts()[class_item]
-            self.class_propability.append((class_item, feature_counts, feature_counts / total_counts))
+            self.class_propability[class_item] = (feature_counts, feature_counts / total_counts)
         
         self.features_propability = {}
         for item in self.features:
@@ -25,15 +25,13 @@ class NaiveBayes():
                 indexes = self.dataset[self.dataset[item] == value].index.tolist()      # Get indexes of the feature value
                 sub_matrix = self.dataset[self.target_attribute].iloc[indexes]          # Get target class values for this feature value
                 sub_matrix_counts = sub_matrix.value_counts()
-                value_classes = {}
+                value_classes = {'yes': 0, 'no': 0}
                 for f_item in range(0, sub_matrix_counts.shape[0]):
                     f_label = sub_matrix_counts.index[f_item]
-                    f_probability = sub_matrix_counts[f_item] / total_counts
-                    value_classes[f_label] = f_probability
-                if not 'yes' in value_classes:
-                    value_classes['yes'] = 0
-                if not 'no' in value_classes:
-                    value_classes['no'] = 0
+                    if f_label == 'yes':
+                        value_classes['yes'] += sub_matrix_counts[f_item] / self.class_propability['yes'][0]
+                    else:
+                        value_classes['no'] += sub_matrix_counts[f_item] / self.class_propability['no'][0]
                 feature_values[value] = value_classes
             self.features_propability[item] = feature_values
 
@@ -48,33 +46,24 @@ class NaiveBayes():
         pnh = self.features_propability['humidity'][humidity]['no']
         pnw = self.features_propability['windy'][windy]['no']
 
-        p_true = pyo * pyt * pyh * pyw
-        p_false = pno * pnt * pnh * pnw
+        p_true = pyo * pyt * pyh * pyw * self.class_propability['yes'][1]
+        p_false = pno * pnt * pnh * pnw * self.class_propability['no'][1]
 
-        # for attribute in self.features_propability:
-        #     f_attributes = self.features_propability[attribute]
-        #     for a_value in f_attributes:
-        #         f_attributes_values = self.features_propability[attribute][a_value]
-        #         if 'yes' in f_attributes_values:
-        #             p_true *= f_attributes_values['yes']
-        #         if 'no' in f_attributes_values:
-        #             p_false *= f_attributes_values['no']
-        
-        p_true *= self.class_propability[1][2]
-        p_false *= self.class_propability[0][2]
+        # Since p(c1) + p(c2) = 1
+        true_class = p_true / (p_true + p_false)
+        false_class = p_false / (p_true + p_false)
 
-        #print(p_true + p_false)
-        if p_true > p_false:
-            print('yes')
+        if true_class > false_class:
+            print('Prediction: yes\nScore: ' + str(true_class))
         else:
-            print('no')
+            print('Prediction: no\nScore: ' + str(false_class))
 
 def main():
     dataset = pd.read_csv("toPlayOrNot.csv")
     
     nb = NaiveBayes(dataset, 'play')
     nb.train()
-    nb.predict('sunny', 'mild', 'high', False)
+    nb.predict('rainy', 'mild', 'high', True)
 
 if __name__ == "__main__":
     main()
